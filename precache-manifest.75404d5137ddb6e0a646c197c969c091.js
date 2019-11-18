90240b130d8812f4be7489eb5bdbb7c46603cdad
gmujica <gregorymujica1993@gmail.com>
2019-11-17 19:20:17 -0500

    Updates

---
+++ precache-manifest.75404d5137ddb6e0a646c197c969c091.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92181eb2230c0178b99801e62c8ed763",
+    "url": "/proyect-ticket-manager/index.html"
+  },
+  {
+    "revision": "6fd0aa4382b5dcc2de65",
+    "url": "/proyect-ticket-manager/static/js/2.977272d0.chunk.js"
+  },
+  {
+    "revision": "3c34c1a67bc826118d73",
+    "url": "/proyect-ticket-manager/static/js/main.0c00e835.chunk.js"
+  },
+  {
+    "revision": "6ebe712168dcc3e7c71c",
+    "url": "/proyect-ticket-manager/static/js/runtime-main.e37b2130.js"
+  }
+]);
